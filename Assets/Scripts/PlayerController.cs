90f8f01f57ac0f266099b4e7062f53faacb5b7cb
shqkr <seokhwan0611@gmail.com>
2025-07-06 14:05:44 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+// PlayerController는 플레이어 캐릭터로서 Player 게임 오브젝트를 제어한다.
+public class PlayerController : MonoBehaviour {
+   public AudioClip deathClip; // 사망 시 재생할 오디오 클립
+    public float jumpForce = 700f; // 점프 힘
+
+   private int jumpCount = 0; // 누적 점프 횟수
+   private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+   private bool isDead = false; // 사망 상태
+
+   private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+   private Animator animator; // 사용할 애니메이터 컴포넌트
+   private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+
+   private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+   private void Update() {
+        // 사망 시 이후 코드를 실행하지 않음
+       if (isDead) return;
+
+       // 마우스 왼쪽 버튼을 눌렀을 시
+       if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+        {
+            jumpCount++;
+            playerRigidbody.linearVelocity = Vector2.zero;
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            playerAudio.Play();
+        }
+       // 위로 상승 중이다가 마우스 왼쪽 버튼에서 손을 때는 순간
+       else if (Input.GetMouseButtonUp(0) && playerRigidbody.linearVelocity.y > 0)
+        {
+            playerRigidbody.linearVelocity = playerRigidbody.linearVelocity * 0.5f;
+        }
+
+        animator.SetBool("Grounded", isGrounded);
+   }
+
+    // 사망 처리
+   private void Die()
+    {
+        animator.SetTrigger("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        playerRigidbody.linearVelocity = Vector2.zero;
+        isDead = true;
+
+        GameManager.instance.OnPlayerDead();
+    }
+
+    // 트리거 콜라이더를 가진 장애물과의 충돌 감지
+   private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Dead" && !isDead)
+        {
+            Die();
+        }
+    }
+
+    // 바닥에 닿았음을 감지하는 처리
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있다면
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
+    }
+
+    // 바닥에서 벗어났음을 감지하는 처리
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Uni-run
+# Uni-run (유니런)
 &lt;레트로의 유니티 게임 프로그래밍 에센스 6> 2번째 실습 프로젝트
+
+## 🎮 Game Overview
+A simple 2D runner game in which the player has to step as many platforms possible to achieve high score.
+
+## 🔧 Tech
+- Unity 6000.0.36f1
+
+## ❗ Notice on Assets
+
+This project includes the Unity-Chan character asset, which is © Unity Technologies Japan.  
+The asset is licensed under the [Unity-Chan License](http://unity-chan.com/contents/license_en/).  
+Used here for non-commercial, educational and portfolio purposes only.
